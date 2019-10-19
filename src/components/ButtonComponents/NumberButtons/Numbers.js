70ae99f9c34141d01f6b7d38b2c3b346e0b1c83d
Iyehvah <Iyehvah@users.noreply.github.com>
2019-10-18 19:31:03 -0500

    I think i have my numbersButton, numbers.js, operatorButtons, operators,js complete now moving onto the specialButton and special.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+import NumberButton from './NumberButton.js';
 //Import your array data to from the provided data file
+import { numbers } from '../../../data.js'
 
-const Numbers = () => {
+
+function LeNumbers() {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+       {numberState.map( numbers => {
+        return(<NumberButton key={numbers}
+           numbers={numbers}/>
+        ); 
+       })}
     </div>
   );
 };
+export default LeNumbers;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { specials } from '../../../data';
 
 //import any components needed
-
+import
 //Import your array data to from the provided data file
 
 const Specials = () => {
